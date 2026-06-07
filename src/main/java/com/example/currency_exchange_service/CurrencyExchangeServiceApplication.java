7e7121943a2042bfa777942bce88f8da39464b55
pawankumar123465 <79195131+pawankumar123465@users.noreply.github.com>
2026-06-07 19:37:12 +0530

    hgjju

--- src/main/java/com/example/currency_exchange_service/CurrencyExchangeServiceApplication.java
+++ src/main/java/com/example/currency_exchange_service/CurrencyExchangeServiceApplication.java
@@ -4,8 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyExchangeServiceApplication
-{
+public class CurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
