33c8faf9023c147359c4307fab259aae3dfa0773
pawankumar123465 <79195131+pawankumar123465@users.noreply.github.com>
2026-06-07 23:26:20 +0530

    fix yml

--- src/main/java/com/example/currency_exchange_service/controller/CurrencyExchangeController.java
+++ src/main/java/com/example/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -1,6 +1,7 @@
 package com.example.currency_exchange_service.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,11 @@ public class CurrencyExchangeController
 	
 	@Autowired
 	private CurrencyExchangeRepository currencyExchangeRepository;
+	@Value("${SONAR_HOST_URL}")
+	private String sonarHostUrl;
+
+	@Value("${SONAR_TOKEN}")
+	private String sonarToken;
 	
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retriveExchange(@PathVariable String from,@PathVariable String to)
@@ -24,7 +30,9 @@ public class CurrencyExchangeController
 		CurrencyExchange currencyExchange= currencyExchangeRepository.findByFromAndTo(from, to);
 		String port=env.getProperty("server.port");
 		currencyExchange.setEnv(port);
-		System.out.println("helloo");
+		System.out.println("sonarHostUrl "+sonarHostUrl);
+		System.out.println("sonarToken "+sonarToken);
+
 		return currencyExchange;
 		
 	}
